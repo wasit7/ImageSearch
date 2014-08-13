c8ee08b9ac57752c163f7201ff006f86973c505c
Taratep <taratep.siraaksorn@gmail.com>
2014-08-13 16:07:03 +0700

    Implement GetL() in LibaryImageDataset

--- RandomForest/src/dataset.py
+++ RandomForest/src/dataset.py
@@ -7,9 +7,11 @@ Created on Wed Aug 06 16:12:37 2014
 from json import loads
 from os import getcwd, listdir
 from os.path import join
+from PIL import Image
+from itertools import permutations
+from time import time
 
 import numpy as np
-L = []
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # Dataset
@@ -31,6 +33,9 @@ class Dataset:
         '''
         Return raw data of record x with dimension theta
         '''
+        raise NotImplementedError
+
+    def getDimension(self):
         raise NotImplementedError
 
     def getSize(self):
@@ -59,26 +64,75 @@ class Rectangle:
 
 class LibraryImageDataset(Dataset):
     def __init__(self, **kwargs):
-        global L
+        self.rectL = []
+        # self.samples = self.getSamples()
 
+        # load JSON
         if 'json' in kwargs:
             json = kwargs['json']
         else :
             json = join('..', '..', 'App', 'json')
 
-        for index, f in  enumerate(listdir(json)):
+        all_file = listdir(json)
+        if not isinstance(all_file, list):
+            all_file = [all_file]
+
+        # length of dataset
+        self.lenDataset = len(all_file)
+        # dimension of dataset
+        print all_file
+
+        self.all_colors = ['rg' 'rb', 'gb', 'rbg', 'brg', 'grb']
+        for index, f in  enumerate(all_file):
             path = join(json, f)
+            
+            # setup L
             with open(path, 'r') as fp:
                 data = loads(fp.read())
+            print data
             rects = []
             for label in data['labels']:
                 rects.append(Rectangle(label=label['label'], x=label['x'],\
                     y=label['y'], w=label['w'], h=label['h']))
-            L.append(rects)
+            self.rectL.append(rects)
+            
+            # setup I
+            # img = Image.open(data['path'])
+            # w, h = img.size 
+            # colors = {}
+            # for color in self.all_colors[:]:
+            #     colors[color] = []
+            # for y in range(h):
+            #     for color in colors:
+            #         colors[color].append([])
+            #     for x in range(w):
+            #         pix = img.getpixel((x,y))
+            #         target_color = self.find_color(*pix)
+            #         for color in colors:
+            #             if color in target_color:
+            #                 colors[color][-1].append(1)
+            #             else :
+            #                 colors[color][-1].append(0)
 
-    def getL(self, x, y, img):
-        global L
-        for rect in L[img]:
+            # for color in colors:
+            #     colors[color] = np.array(colors[color])
+            #     integral_image = np.cumsum(np.cumsum(colors[color], axis=0), axis=1)
+            #     print integral_image
+
+    def find_color(self, r, g, b):
+        colors = []
+        for i, color in enumerate(self.all_colors[:]):
+            if i < 3:
+                exec('res = %s > %s'%(color[0], color[1]))
+            else :
+                exec('res = %s**2 > %s*%s'%(color[0], color[1], color[2]))
+            if res :
+                colors.append(color)
+        return colors   
+
+    def getL(x) :
+        x, y, img = self.samples[x]
+        for rect in self.L[img]:
             if (x,y) in rect :
                 return rect.label
         return 0
@@ -97,9 +151,9 @@ class SpiralDataset(Dataset):
         self.clmax = clmax;    # class max of dataset
         self.data_per_class = data_per_class    # q size per class per client
         
-        self.dimension = 2 # it is axis x and y
+        dimension = 2 # it is axis x and y
         
-        self.I =  np.zeros([self.dimension, 0], dtype=np.float)  # np.ndarray row vetor, hold features
+        self.I =  np.zeros([dimension, 0], dtype=np.float)  # np.ndarray row vetor, hold features
         self.L =  np.array([], dtype=np.int)    # np.array, hold label
 
         # create I
@@ -124,6 +178,12 @@ class SpiralDataset(Dataset):
         '''
         return self.I[theta, x]
 
+    def getDimension(self):
+        '''
+        Return dimension of data
+        '''
+        return self.I.shape[0]
+
     def getSize(self):
         '''
         Return size of dataset
@@ -134,12 +194,7 @@ class SpiralDataset(Dataset):
         pass
 
     def getParam(self, X):
-        '''
-        Return list of theta, tau for X
-        '''
-        theta = np.random.randint(self.dimension, size=len(X))
-        tau = self.getI(theta, X)
-        return theta, tau
+        pass
 
     def __str__(self):
         '''
@@ -150,5 +205,5 @@ class SpiralDataset(Dataset):
 
 if __name__ == '__main__':
     dataset = LibraryImageDataset()
-    print L
-    print dataset.getL(0, 0, 1)
+    # print dataset.rectL
+    # print dataset.getL(188, 0, 1)
